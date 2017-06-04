71ef6805e477bf608bc5fc08ea345c1ae2209cb1
cconley <christopher.conley@gmail.com>
2017-06-03 19:08:11 -0700

    gauss modifications

--- lumb-disc/04-lumb-cvvote-spacemap.sh
+++ lumb-disc/04-lumb-cvvote-spacemap.sh
@@ -6,13 +6,13 @@
 ## module load R 
 
 # Name of the job - you'll probably want to customize this.
-#SBATCH --job-name=her2-05
+#SBATCH --job-name=LumB-04
 # Tell Gauss how much memory per CPU your job will use:
-#SBATCH --mem=30120
+#SBATCH --mem=30220
 
 # Standard out and Standard Error output files with the job number in the name.
-#SBATCH -o /home/cconley/scratch-data/neta-metabric/disc-cv-vote/her2/05/log.out
-#SBATCH -e /home/cconley/scratch-data/neta-metabric/disc-cv-vote/her2/05/log.err
+#SBATCH -o /home/cconley/scratch-data/neta-metabric/disc-cv-vote/lumb/04/log.out
+#SBATCH -e /home/cconley/scratch-data/neta-metabric/disc-cv-vote/lumb/04/log.err
 
 #SBATCH --nodes=4
 #SBATCH --ntasks-per-node=31
@@ -31,4 +31,4 @@ hostname
 
 # Execute each of the jobs with a different index (the R script will then process
 # this to do something different for each index):
-/usr/bin/R --no-save --no-restore --no-site-file --no-init-file --args 31 < /home/cconley/repos/neta-metabric/her2-disc/05-her2-cvvote-spacemap.R
+/usr/bin/R --no-save --no-restore --no-site-file --no-init-file --args 31 < /home/cconley/repos/neta-metabric/lumb-disc/04-lumb-cvvote-spacemap.R
